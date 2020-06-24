e4fba8b7e35a2afc3d937e7c1d92724cce829bb7
Vladimir Podolyan <vpodolyan@outlook.com>
2020-06-24 19:47:31 +0300

    Add project boilerplate

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <Currencies />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Currencies from "./components/Currencies.vue";
+
+@Component({
+  components: {
+    Currencies
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Currencies.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+    div {{rates}}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Currencies extends Vue {
+  private rates: Array<{ [currency: string]: number }> = [];
+
+  mounted() {
+    fetch("https://api.exchangeratesapi.io/latest")
+      .then(stream => stream.json())
+      .then(data => (this.rates = data.rates));
+  }
+}
+</script>
+
+<style scoped></style>
